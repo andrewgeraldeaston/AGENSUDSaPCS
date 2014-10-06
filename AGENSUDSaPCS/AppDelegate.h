f1dc1b30aa38b2c1a6af19dab39351effe93cc49
Andrew Easton <andrewgeraldeaston@andrews-air.home>
2014-10-06 14:15:30 +0100

    Initial Commit

---
+++ AGENSUDSaPCS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AGENSUDSaPCS
+//
+//  Created by Andrew Easton on 06/10/2014.
+//  Copyright (c) 2014 IOS7Course-age. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AGENSUDSaPCS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGENSUDSaPCS
+//
+//  Created by Andrew Easton on 06/10/2014.
+//  Copyright (c) 2014 IOS7Course-age. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
